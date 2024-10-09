bef6f88decaa29c66d2ac88029d1719cf4ad8b03
MxxApp <medyx@foxmail.com>
2024-10-09 04:10:12 +0000

    🎉auto update by Gmeek action

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# 梦想乡 :link: https://mxx.app 
+# MXX.APP :link: https://mxx.app 
 ### :page_facing_up: [2](https://mxx.app/tag.html) 
 ### :speech_balloon: 0 
 ### :hibiscus: 5705 
-### :alarm_clock: 2024-10-09 10:49:22 
+### :alarm_clock: 2024-10-09 12:10:12 
 ### Powered by :heart: [Gmeek](https://github.com/Meekdai/Gmeek)
--- docs/plugins/copyright.js
+++ docs/plugins/copyright.js
@@ -1,10 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const metaURL = document.querySelector('meta[property="og:url"]');
-    const originalURL = metaURL ? metaURL.getAttribute('content') : window.location.href;
     const targetElement = document.querySelector('div[style*="float:right;"]');
     if (targetElement && targetElement.textContent === '转载文章请注明来自 MXX.APP') {
         targetElement.style.float = 'left';
-        
+        const currentURL = window.location.href;
         targetElement.innerHTML = `
             <p>本博客所有文章除特别声明外，均采用&nbsp;<a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank">CC BY-NC-SA 4.0</a>&nbsp;许可协议。转载请注明来自&nbsp;<a href="https://mxx.app" target="_blank">MXX.APP</a></p>
             <p>原文链接：<a href="${currentURL}" target="_blank">${currentURL}</a></p>
