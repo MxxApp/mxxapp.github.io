01d03f5a53d4445d0712cff04e1bf83a496cd290
Mxx <i.dyx@qq.com>
2024-10-26 15:14:26 +0800

    Create gettime.js

---
+++ static/plugins/gettime.js
@@ -0,0 +1,14 @@
+    <script>
+        // 获取当前时间戳
+        const timestamp = new Date().getTime();
+
+        // 创建并添加第一个脚本标签
+        const script1 = document.createElement('script');
+        script1.src = '/plugins/articletoc.js?t=' + timestamp;
+        document.head.appendChild(script1);
+
+        // 创建并添加第二个脚本标签
+        const script2 = document.createElement('script');
+        script2.src = '/plugins/lightbox.js?t=' + timestamp;
+        document.head.appendChild(script2);
+    </script>
